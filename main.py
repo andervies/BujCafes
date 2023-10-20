17f38cb6785efb7a3ed71479e37659461e138614
Ander <anderclace@gmail.com>
2023-10-20 16:47:32 +0100

    initial commit: navbar template, simple flask app with database configuration

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+
+
+# Creating the database model
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cafe_name = db.Column(db.String, unique=True, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    open = db.Column(db.DateTime)
+    close = db.Column(db.DateTime)
+    coffee_rating = db.Column(db.Numeric, nullable=False)
+    wifi_rating = db.Column(db.Numeric, nullable=False)
+    power_rating = db.Column(db.Numeric, nullable=False)
+
+
+@app.route("/")
+def home():
+    return "hello world"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
